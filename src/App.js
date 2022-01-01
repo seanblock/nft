7c6dda44bd2766df3896728c9bbaedb8efd9ca9c
Sean Block <42530814+seanblock@users.noreply.github.com>
2022-01-01 11:12:38 -0500

    more fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Cube from './components/Cube';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  
+  const zoomIn = () => {
+    document.documentElement.style.setProperty('--cube', '300px');
+  }
+
   return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">NFT Gallery</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
     <div id="header">
-      
+      <div id="wall">
+        <div className="row2">
+          <Cube color="dark" season="01" name="The Dog" nft="dog" number="0001"
+           onClick={zoomIn()}/>
+          <Cube color="red" season="01" name="The Cat" nft="cat" number="0005"/>
+          <Cube color="red" season="01" name="The Cat" nft="cat" number="0005"/>
+        </div>
+        <div className="row2">
+          <Cube color="blue" season="01" name="The Cat" nft="cat" number="0003"/>
+          <Cube color="blue" season="01" name="The Cat" nft="cat" number="0002"/>
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0002"/>
+        </div>
+        <div className="row2">
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+          <Cube color="red" season="01" name="The Cat" nft="cat" number="0001"/>
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+        </div>
+        <div className="row2">
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+        </div>
+        <div className="row2">
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+          <Cube color="dark" season="01" name="The Cat" nft="cat" number="0001"/>
+        </div>
+      </div>
     </div>
+    </>
   );
 }
 
---
+++ src/components/Cube.js
@@ -0,0 +1,56 @@
+import { Col, Row } from "react-bootstrap";
+import qr from '../img/wallet.png'
+import dog from '../img/dog.png'
+import cat from '../img/cat.png'
+import { useState } from "react";
+
+const Cube = ({color, season, name, nft, number}) => {
+
+    const nftItem = () => {
+        if (nft == "dog") {
+            return(<img src={dog} alt="initial" className="starter"/>)
+        } else {
+            return(<img src={cat} alt="initial" className="starter"/>)
+        }
+    }
+    const [hover, setHover] = useState(false);
+
+    return ( 
+    
+    <div className={`cube ${hover ? "cube2":""}`} 
+        onMouseEnter={()=>{setHover(true)}}
+        onMouseLeave={()=>{setHover(false)}}
+    >
+        {nftItem(nft)}
+        <div className={`cube-front ${hover ? "cube-front2":""}`}>
+            <div 
+                className={`glass-door ${hover ? "glass-door2":""}`}
+            ></div>
+            <span className={`item-info ${hover ? "item-info2": ""}`}>
+                <div className="item-row">
+                    <div className="item-season">
+                        <h4>SEASON</h4>
+                        <h1>{season}</h1>
+                    </div>
+                    <div className="item-qr">
+                        <img src={qr} alt="QR code"/>
+                    </div>
+                </div>
+                <div className="row2">
+                    <div className="item-name">
+                        <h3>{name} <span className="number">#{number}</span></h3>
+                    </div>
+                </div>
+            </span>
+        </div>
+        <div className={`cube-back ${color}-box`}></div>
+        <div className={`cube-left ${color}-box`}></div>
+        <div className={`cube-right ${color}-box`}></div>
+        <div className={`cube-top ${color}-box`}></div>
+        <div className={`cube-bottom ${color}-box`}></div>
+    </div>
+    
+    );
+}
+ 
+export default Cube;
